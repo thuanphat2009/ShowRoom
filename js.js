d427b52181770e0b276a28fadb4ac1a37cb3d8f5
thuanphat2009 <anhxanh2019@gmail.com>
2021-09-09 20:01:58 +0700

    closebtn

--- js.js
+++ js.js
@@ -3,8 +3,13 @@ const $$ = document.querySelector.bind(document)
 
 const btnMenu = $('.menu-minibox-icon')
 const subMenu = $('.overlay')
+const closeMenu = $('.close-menu-icon')
 
 
 btnMenu.onclick = function(){
     subMenu.classList.add('active')
+}
+
+closeMenu.onclick = function(){
+    subMenu.classList.remove('active')
 }
